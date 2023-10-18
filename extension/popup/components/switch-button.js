e54a341f2b1f2ff229a7ef5992caed5ec009d338
Andria Brown <andria_girl@pm.me>
2023-10-18 02:09:30 -0400

    prettier formatting

--- extension/popup/components/switch-button.js
+++ extension/popup/components/switch-button.js
@@ -1,201 +1,203 @@
 import { LitElement, css, html } from "lit";
 
 export class SwitchButton extends LitElement {
-  static formAssociated = true;
+	static formAssociated = true;
 
-  static properties = {
-    enabled: {
-      type: Boolean,
-      reflect: true,
-    },
-  };
+	static properties = {
+		enabled: {
+			type: Boolean,
+			reflect: true,
+		},
+	};
 
-  static styles = css`
-    :host {
-      --border-gradient-angle: 128deg;
-      --border-gradient-first-color: #c3d9ee;
-      --border-gradient-first-stop: 24%;
-      --border-gradient-second-color: #a1b3c4;
-      --border-gradient-second-stop: 78.3%;
+	static styles = css`
+		:host {
+			--border-gradient-angle: 128deg;
+			--border-gradient-first-color: #c3d9ee;
+			--border-gradient-first-stop: 24%;
+			--border-gradient-second-color: #a1b3c4;
+			--border-gradient-second-stop: 78.3%;
 
-      position: relative;
-      display: flex;
-      flex-direction: row-reverse;
-      justify-content: center;
-      align-items: center;
-      gap: 11px;
+			position: relative;
+			display: flex;
+			flex-direction: row-reverse;
+			justify-content: center;
+			align-items: center;
+			gap: 11px;
 
-      cursor: pointer;
-      user-select: none;
-      font-size: 0.875rem;
-      font-family: "Nunito Sans", Arial, Helvetica, sans-serif;
-      font-weight: bold;
-      color: hsl(201deg 17% 34%);
+			cursor: pointer;
+			user-select: none;
+			font-size: 0.875rem;
+			font-family: "Nunito Sans", Arial, Helvetica, sans-serif;
+			font-weight: bold;
+			color: hsl(201deg 17% 34%);
 
-      box-sizing: content-box;
-      width: min-content;
-      padding-block: 7px;
-      padding-inline: 10px 16px;
+			box-sizing: content-box;
+			width: min-content;
+			padding-block: 7px;
+			padding-inline: 10px 16px;
 
-      border-radius: 999px;
-      border: solid 2px transparent;
-      background: linear-gradient(149deg, #fff 16.83%, #e4f0fa 83.63%)
-          padding-box,
-        linear-gradient(
-            var(--border-gradient-angle),
-            var(--border-gradient-first-color) var(--border-gradient-first-stop),
-            var(--border-gradient-second-color)
-              var(--border-gradient-second-stop)
-          )
-          border-box;
-      box-shadow: 0px 4px 9px 0px #c8d4e2,
-        -5px -4px 5px 0px rgba(255, 255, 255, 0.75), 0px 3px 7px 0px #c9d3e2;
+			border-radius: 999px;
+			border: solid 2px transparent;
+			background:
+				linear-gradient(149deg, #fff 16.83%, #e4f0fa 83.63%) padding-box,
+				linear-gradient(var(--border-gradient-angle), var(--border-gradient-first-color) var(--border-gradient-first-stop), var(--border-gradient-second-color) var(--border-gradient-second-stop)) border-box;
+			box-shadow:
+				0px 4px 9px 0px #c8d4e2,
+				-5px -4px 5px 0px rgba(255, 255, 255, 0.75),
+				0px 3px 7px 0px #c9d3e2;
 
-      transition: box-shadow 0.2s ease-out,
-        --border-gradient-angle 0.2s ease-out,
-        --border-gradient-first-color 0.2s ease-out,
-        --border-gradient-first-stop 0.2s ease-out,
-        --border-gradient-second-color 0.2s ease-out,
-        --border-gradient-second-stop 0.2s ease-out;
-    }
-    :host(:hover) {
-      box-shadow: 0px 4px 9px 0px rgba(200, 212, 226, 0.65),
-        -5px -4px 5px 0px rgba(255, 255, 255, 0.6),
-        0px 3px 7px 0px rgba(201, 211, 226, 0.65);
-    }
-    :host(:active) {
-      box-shadow: 0px 1px 9px 0px #c8d4e2 inset, 0px 3px 7px 0px #c9d3e2 inset,
-        -5px -4px 5px 0px rgba(255, 255, 255, 0.75) inset;
-    }
-    :host(:focus-visible) {
-      outline: none;
-      --border-gradient-angle: 128deg;
-      --border-gradient-first-color: #949cea;
-      --border-gradient-first-stop: 24%;
-      --border-gradient-second-color: #7881e3;
-      --border-gradient-second-stop: 78.3%;
-    }
+			transition:
+				box-shadow 0.2s ease-out,
+				--border-gradient-angle 0.2s ease-out,
+				--border-gradient-first-color 0.2s ease-out,
+				--border-gradient-first-stop 0.2s ease-out,
+				--border-gradient-second-color 0.2s ease-out,
+				--border-gradient-second-stop 0.2s ease-out;
+		}
+		:host(:hover) {
+			box-shadow:
+				0px 4px 9px 0px rgba(200, 212, 226, 0.65),
+				-5px -4px 5px 0px rgba(255, 255, 255, 0.6),
+				0px 3px 7px 0px rgba(201, 211, 226, 0.65);
+		}
+		:host(:active) {
+			box-shadow:
+				0px 1px 9px 0px #c8d4e2 inset,
+				0px 3px 7px 0px #c9d3e2 inset,
+				-5px -4px 5px 0px rgba(255, 255, 255, 0.75) inset;
+		}
+		:host(:focus-visible) {
+			outline: none;
+			--border-gradient-angle: 128deg;
+			--border-gradient-first-color: #949cea;
+			--border-gradient-first-stop: 24%;
+			--border-gradient-second-color: #7881e3;
+			--border-gradient-second-stop: 78.3%;
+		}
 
-    #indicator {
-      display: inline-block;
-      --size: calc(18 / 14 * 1em);
-      width: var(--size);
-      height: var(--size);
+		#indicator {
+			display: inline-block;
+			--size: calc(18 / 14 * 1em);
+			width: var(--size);
+			height: var(--size);
 
-      border-radius: 100%;
-      background: linear-gradient(90deg, #ff6b59 -8.26%, #d35646 144.55%);
-      box-shadow: 6px 9px 40px 0px #ff8677,
-        -6px -6px 16px 0px rgba(255, 255, 255, 0.6);
+			border-radius: 100%;
+			background: linear-gradient(90deg, #ff6b59 -8.26%, #d35646 144.55%);
+			box-shadow:
+				6px 9px 40px 0px #ff8677,
+				-6px -6px 16px 0px rgba(255, 255, 255, 0.6);
 
-      transform: rotate(90deg);
-    }
-    #state {
-      display: inline-grid;
-      grid-template-columns: 1fr;
-      text-align: center;
-    }
-    #state slot {
-      display: inline;
-      grid-area: 1 / 1;
-    }
+			transform: rotate(90deg);
+		}
+		#state {
+			display: inline-grid;
+			grid-template-columns: 1fr;
+			text-align: center;
+		}
+		#state slot {
+			display: inline;
+			grid-area: 1 / 1;
+		}
 
-    :host(:--enabled) {
-      flex-direction: row;
-      padding-inline: 16px 10px;
-    }
-    :host(:--enabled) #indicator {
-      background: linear-gradient(90deg, #59ff7e -8.26%, #46d365 144.55%);
-      box-shadow: 6px 9px 40px 0px #77ff85,
-        -6px -6px 16px 0px rgba(255, 255, 255, 0.6);
-    }
-    :host(:not(:--enabled)) #state slot[name="on"] {
-      opacity: 0;
-    }
-    :host(:--enabled) #state slot[name="off"] {
-      opacity: 0;
-    }
+		:host(:--enabled) {
+			flex-direction: row;
+			padding-inline: 16px 10px;
+		}
+		:host(:--enabled) #indicator {
+			background: linear-gradient(90deg, #59ff7e -8.26%, #46d365 144.55%);
+			box-shadow:
+				6px 9px 40px 0px #77ff85,
+				-6px -6px 16px 0px rgba(255, 255, 255, 0.6);
+		}
+		:host(:not(:--enabled)) #state slot[name="on"] {
+			opacity: 0;
+		}
+		:host(:--enabled) #state slot[name="off"] {
+			opacity: 0;
+		}
 
-    :host(.transitioning) {
-      view-transition-name: switch-button;
-    }
-    :host(.transitioning) #indicator {
-      view-transition-name: switch-button-indicator;
-    }
-    :host(.transitioning) #state {
-      view-transition-name: switch-button-state;
-    }
-  `;
+		:host(.transitioning) {
+			view-transition-name: switch-button;
+		}
+		:host(.transitioning) #indicator {
+			view-transition-name: switch-button-indicator;
+		}
+		:host(.transitioning) #state {
+			view-transition-name: switch-button-state;
+		}
+	`;
 
-  #internals;
-  constructor() {
-    super();
+	#internals;
+	constructor() {
+		super();
 
-    this.#internals = this.attachInternals();
-    this.#internals.role = "switch";
-    this.#internals.ariaChecked = "false";
+		this.#internals = this.attachInternals();
+		this.#internals.role = "switch";
+		this.#internals.ariaChecked = "false";
 
-    if (!this.hasAttribute("tabindex")) this.tabIndex = 0;
+		if (!this.hasAttribute("tabindex")) this.tabIndex = 0;
 
-    this.enabled = false;
-  }
+		this.enabled = false;
+	}
 
-  #handleClick() {
-    this.enabled = !this.enabled;
-  }
-  #handleKeyUp({ key }) {
-    switch (key) {
-      case " ":
-      case "Enter":
-        this.enabled = !this.enabled;
-        break;
-      default:
-        break;
-    }
-  }
+	#handleClick() {
+		this.enabled = !this.enabled;
+	}
+	#handleKeyUp({ key }) {
+		switch (key) {
+			case " ":
+			case "Enter":
+				this.enabled = !this.enabled;
+				break;
+			default:
+				break;
+		}
+	}
 
-  connectedCallback() {
-    super.connectedCallback();
-    this.addEventListener("click", this.#handleClick);
-    this.addEventListener("keyup", this.#handleKeyUp);
-  }
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    this.removeEventListener("click", this.#handleClick);
-    this.removeEventListener("keyup", this.#handleKeyUp);
-  }
+	connectedCallback() {
+		super.connectedCallback();
+		this.addEventListener("click", this.#handleClick);
+		this.addEventListener("keyup", this.#handleKeyUp);
+	}
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		this.removeEventListener("click", this.#handleClick);
+		this.removeEventListener("keyup", this.#handleKeyUp);
+	}
 
-  willUpdate(changedProperties) {
-    if (changedProperties.has("enabled")) {
-      this.classList.add("transitioning");
-      const viewTransition = document.startViewTransition(async () => {
-        if (this.enabled) this.#internals.states.add("--enabled");
-        else this.#internals.states.delete("--enabled");
-        await this.updateComplete;
-      });
+	willUpdate(changedProperties) {
+		if (changedProperties.has("enabled")) {
+			this.classList.add("transitioning");
+			const viewTransition = document.startViewTransition(async () => {
+				if (this.enabled) this.#internals.states.add("--enabled");
+				else this.#internals.states.delete("--enabled");
+				await this.updateComplete;
+			});
 
-      viewTransition.finished.then(async () => {
-        this.classList.remove("transitioning");
-        this.#internals.setFormValue(this.enabled ? "on" : "off", "enabled");
-        this.#internals.ariaChecked = this.enabled;
-        this.dispatchEvent(
-          new Event("change", {
-            bubbles: true,
-            composed: true,
-            cancelable: false,
-          })
-        );
-      });
-    }
-  }
-  render() {
-    return html`
-      <div id="state" part="state">
-        <slot name="off" aria-hidden=${this.enabled}>OFF</slot>
-        <slot name="on" aria-hidden=${!this.enabled}>ON</slot>
-      </div>
-      <div id="indicator" part="indicator"></div>
-    `;
-  }
+			viewTransition.finished.then(async () => {
+				this.classList.remove("transitioning");
+				this.#internals.setFormValue(this.enabled ? "on" : "off", "enabled");
+				this.#internals.ariaChecked = this.enabled;
+				this.dispatchEvent(
+					new Event("change", {
+						bubbles: true,
+						composed: true,
+						cancelable: false,
+					}),
+				);
+			});
+		}
+	}
+	render() {
+		return html`
+			<div id="state" part="state">
+				<slot name="off" aria-hidden=${this.enabled}>OFF</slot>
+				<slot name="on" aria-hidden=${!this.enabled}>ON</slot>
+			</div>
+			<div id="indicator" part="indicator"></div>
+		`;
+	}
 }
 
 customElements.define("switch-button", SwitchButton);
--- extension/scripts/content-script.js
+++ extension/scripts/content-script.js
@@ -4,18 +4,18 @@
 //const AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var prefs = {
-  sites: {
-    default: {
-      enabled: false,
-      threshold: -24,
-      knee: 30,
-      ratio: 12,
-      attack: 0.003,
-      release: 0.25,
-      boost: 0,
-    },
-  },
-  //disableForCrossSiteElements: false
+	sites: {
+		default: {
+			enabled: false,
+			threshold: -24,
+			knee: 30,
+			ratio: 12,
+			attack: 0.003,
+			release: 0.25,
+			boost: 0,
+		},
+	},
+	//disableForCrossSiteElements: false
 };
 var settings = prefs.sites.default;
 var parameterChangeDuration = 0.1;
@@ -25,29 +25,29 @@ var logPrefix = "Audio Compressor: ";
 //console.log(logPrefix + 'injecting compressor');
 
 window.audioCompressor = {
-  targets: [],
+	targets: [],
 };
 
 function adjustSource(target, settings) {
-  /*
+	/*
   document.querySelectorAll('video, audio').forEach((e) => {
     e.crossOrigin = 'anonymous';
   });
   */
 
-  if (typeof target === "undefined") return;
-  if (target == null) return;
+	if (typeof target === "undefined") return;
+	if (target == null) return;
 
-  if (!window.audioCompressor.targets.includes(target)) {
-    window.audioCompressor.targets.push(target);
-  }
+	if (!window.audioCompressor.targets.includes(target)) {
+		window.audioCompressor.targets.push(target);
+	}
 
-  if (typeof target.attached === "undefined") {
-    target.attached = false;
-  }
+	if (typeof target.attached === "undefined") {
+		target.attached = false;
+	}
 
-  if (!target.attached && settings.enabled) {
-    /*
+	if (!target.attached && settings.enabled) {
+		/*
     var targetURL;
     try {
       targetURL = new URL(target.currentSrc);
@@ -64,223 +64,202 @@ function adjustSource(target, settings) {
     }
     */
 
-    if (!target.initialized) {
-      console.log(logPrefix + "creating compressor", settings);
-      //console.log(logPrefix + 'creating compressor', settings, target);
+		if (!target.initialized) {
+			console.log(logPrefix + "creating compressor", settings);
+			//console.log(logPrefix + 'creating compressor', settings, target);
 
-      //target.crossOrigin = 'anonymous';
+			//target.crossOrigin = 'anonymous';
 
-      target.context = new AudioContext();
-      target.source = target.context.createMediaElementSource(target);
-      target.compressor = target.context.createDynamicsCompressor();
-      target.boost = target.context.createGain();
-      target.initialized = true;
-    }
+			target.context = new AudioContext();
+			target.source = target.context.createMediaElementSource(target);
+			target.compressor = target.context.createDynamicsCompressor();
+			target.boost = target.context.createGain();
+			target.initialized = true;
+		}
 
-    try {
-      target.source.disconnect();
-    } catch (e) {
-      console.log(logPrefix + "caught error disconnecting source", e);
-    }
+		try {
+			target.source.disconnect();
+		} catch (e) {
+			console.log(logPrefix + "caught error disconnecting source", e);
+		}
 
-    target.source.connect(target.compressor);
-    target.compressor.connect(target.boost);
-    target.boost.connect(target.context.destination);
+		target.source.connect(target.compressor);
+		target.compressor.connect(target.boost);
+		target.boost.connect(target.context.destination);
 
-    applySettings();
-    target.attached = true;
-  } else if (target.attached && !settings.enabled) {
-    console.log(logPrefix + "disabling compressor");
+		applySettings();
+		target.attached = true;
+	} else if (target.attached && !settings.enabled) {
+		console.log(logPrefix + "disabling compressor");
 
-    target.source.disconnect(target.compressor);
-    target.compressor.disconnect(target.boost);
-    target.boost.disconnect(target.context.destination);
-    target.source.connect(target.context.destination);
-    target.attached = false;
-  } else if (target.attached && settings.enabled) {
-    applySettings();
-  }
+		target.source.disconnect(target.compressor);
+		target.compressor.disconnect(target.boost);
+		target.boost.disconnect(target.context.destination);
+		target.source.connect(target.context.destination);
+		target.attached = false;
+	} else if (target.attached && settings.enabled) {
+		applySettings();
+	}
 
-  function applySettings() {
-    for (var s in settings) {
-      var value = settings[s];
+	function applySettings() {
+		for (var s in settings) {
+			var value = settings[s];
 
-      if (s == "enabled") {
-      } else if (s == "boost") {
-        try {
-          target.boost.gain.exponentialRampToValueAtTime(
-            value * 4 + 1,
-            parameterChangeDuration
-          );
-        } catch (e) {
-          console.log(logPrefix + "error setting gain", e);
-          target.boost.gain.value = value * 4 + 1;
-        }
-      } else {
-        try {
-          if (s == "knee" || s == "attack" || s == "release") {
-            if (value <= 0) {
-              value = 0.001;
-            }
-          }
+			if (s == "enabled") {
+			} else if (s == "boost") {
+				try {
+					target.boost.gain.exponentialRampToValueAtTime(value * 4 + 1, parameterChangeDuration);
+				} catch (e) {
+					console.log(logPrefix + "error setting gain", e);
+					target.boost.gain.value = value * 4 + 1;
+				}
+			} else {
+				try {
+					if (s == "knee" || s == "attack" || s == "release") {
+						if (value <= 0) {
+							value = 0.001;
+						}
+					}
 
-          if (s == "threshold") {
-            target.compressor[s].linearRampToValueAtTime(
-              value,
-              parameterChangeDuration
-            );
-          } else {
-            target.compressor[s].exponentialRampToValueAtTime(
-              value,
-              parameterChangeDuration
-            );
-          }
-        } catch (e) {
-          console.log(logPrefix + "error setting " + s, e);
-          target.compressor[s].value = value;
-        }
-      }
-    }
-  }
+					if (s == "threshold") {
+						target.compressor[s].linearRampToValueAtTime(value, parameterChangeDuration);
+					} else {
+						target.compressor[s].exponentialRampToValueAtTime(value, parameterChangeDuration);
+					}
+				} catch (e) {
+					console.log(logPrefix + "error setting " + s, e);
+					target.compressor[s].value = value;
+				}
+			}
+		}
+	}
 
-  if (typeof browser === "undefined") {
-    window.browser = chrome;
-  }
-  browser.runtime.sendMessage({ active: target.attached == true });
+	if (typeof browser === "undefined") {
+		window.browser = chrome;
+	}
+	browser.runtime.sendMessage({ active: target.attached == true });
 }
 
 function getBestSiteMatch() {
-  var bestSiteMatch = "default";
-  var maxLength = 0;
-  for (var s in prefs.sites) {
-    if (s == "default") {
-      continue;
-    }
-    if (document.location.href.startsWith(s) && s.length > maxLength) {
-      bestSiteMatch = s;
-      maxLength = s.length;
-    }
-  }
+	var bestSiteMatch = "default";
+	var maxLength = 0;
+	for (var s in prefs.sites) {
+		if (s == "default") {
+			continue;
+		}
+		if (document.location.href.startsWith(s) && s.length > maxLength) {
+			bestSiteMatch = s;
+			maxLength = s.length;
+		}
+	}
 
-  return bestSiteMatch;
+	return bestSiteMatch;
 }
 
 var update = (target) => {
-  if (typeof window.audioCompressor == "undefined") return;
+	if (typeof window.audioCompressor == "undefined") return;
 
-  let settings = prefs.sites[getBestSiteMatch()];
+	let settings = prefs.sites[getBestSiteMatch()];
 
-  window.audioCompressor.targets.forEach((t) => {
-    console.log(logPrefix + "updating target", t);
-    adjustSource(t, settings);
-  });
+	window.audioCompressor.targets.forEach((t) => {
+		console.log(logPrefix + "updating target", t);
+		adjustSource(t, settings);
+	});
 
-  if (target == null) {
-    let videos = document.querySelectorAll("video");
-    for (var v of videos) {
-      let playing = !!(
-        v.currentTime > 0 &&
-        !v.paused &&
-        !v.ended &&
-        v.readyState > 2 &&
-        !v.muted
-      );
-      if (!playing) continue;
-      if (window.audioCompressor.targets.includes(v)) continue;
+	if (target == null) {
+		let videos = document.querySelectorAll("video");
+		for (var v of videos) {
+			let playing = !!(v.currentTime > 0 && !v.paused && !v.ended && v.readyState > 2 && !v.muted);
+			if (!playing) continue;
+			if (window.audioCompressor.targets.includes(v)) continue;
 
-      target = v;
-      break;
-    }
-  }
-  if (target == null) return;
+			target = v;
+			break;
+		}
+	}
+	if (target == null) return;
 
-  if (!window.audioCompressor.targets.includes(target)) {
-    console.log(logPrefix + "adding target", target);
-    adjustSource(target, settings);
-  }
+	if (!window.audioCompressor.targets.includes(target)) {
+		console.log(logPrefix + "adding target", target);
+		adjustSource(target, settings);
+	}
 };
 
 chrome.storage.local.get(prefs, (results) => {
-  Object.assign(prefs, results);
-  update();
+	Object.assign(prefs, results);
+	update();
 });
 
 chrome.storage.onChanged.addListener((changes) => {
-  /*
+	/*
   if (changes.disableForCrossSiteElements) {
     prefs.disableForCrossSiteElements = changes.disableForCrossSiteElements.newValue == true;
   }
   */
-  if (changes.sites) {
-    prefs.sites = changes.sites.newValue;
-    update();
-  }
+	if (changes.sites) {
+		prefs.sites = changes.sites.newValue;
+		update();
+	}
 });
 
 window.addEventListener(
-  "playing",
-  ({ target }) => {
-    update(target);
-  },
-  true
+	"playing",
+	({ target }) => {
+		update(target);
+	},
+	true,
 );
 
 window.addEventListener(
-  "canplay",
-  ({ target }) => {
-    update(target);
-  },
-  true
+	"canplay",
+	({ target }) => {
+		update(target);
+	},
+	true,
 );
 
 setInterval(() => {
-  let videos = document.querySelectorAll("video");
-  videos.forEach((v) => {
-    let playing = !!(
-      v.currentTime > 0 &&
-      !v.paused &&
-      !v.ended &&
-      v.readyState > 2 &&
-      !v.muted
-    );
-    if (!playing) return;
-    if (window.audioCompressor.targets.includes(v)) return;
-    update(v);
-  });
+	let videos = document.querySelectorAll("video");
+	videos.forEach((v) => {
+		let playing = !!(v.currentTime > 0 && !v.paused && !v.ended && v.readyState > 2 && !v.muted);
+		if (!playing) return;
+		if (window.audioCompressor.targets.includes(v)) return;
+		update(v);
+	});
 }, 500);
 
 if (typeof play === "undefined") {
-  const play = Audio.prototype.play;
+	const play = Audio.prototype.play;
 }
 
 Audio.prototype.play = function () {
-  try {
-    update(this);
-  } catch (e) {
-    console.log(logPrefix, e);
-  }
-  return play.apply(this, arguments);
+	try {
+		update(this);
+	} catch (e) {
+		console.log(logPrefix, e);
+	}
+	return play.apply(this, arguments);
 };
 
 if (typeof browser === "undefined") {
-  window.browser = chrome;
+	window.browser = chrome;
 }
 browser.runtime.onMessage.addListener(() => {
-  if (window.audioCompressor == null) {
-    return;
-  }
-  if (window.audioCompressor.targets == null) {
-    return;
-  }
+	if (window.audioCompressor == null) {
+		return;
+	}
+	if (window.audioCompressor.targets == null) {
+		return;
+	}
 
-  var active = false;
-  for (var t of window.audioCompressor.targets) {
-    if (t == null) continue;
-    if (t.attached) {
-      active = true;
-      break;
-    }
-  }
+	var active = false;
+	for (var t of window.audioCompressor.targets) {
+		if (t == null) continue;
+		if (t.attached) {
+			active = true;
+			break;
+		}
+	}
 
-  chrome.runtime.sendMessage({ active: active });
+	chrome.runtime.sendMessage({ active: active });
 });
